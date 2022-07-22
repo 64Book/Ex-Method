88fae10771eeeb5b657a80388233f1358fcaf5f3
64Book <booknarawich2001@gmail.com>
2022-07-22 15:51:54 +0700

    Ex Method

---
+++ src/Ex3/Ex3.java
@@ -0,0 +1,21 @@
+package Ex3;
+
+class Wage {
+    //Method Wage
+    public int hour;
+    public float rate;
+    public float calWage(){
+        float total = (40 * rate) + (hour - 40) * (1.5f * rate);
+        return total;
+    }
+}
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Wage emp_wage = new Wage();
+        emp_wage.hour = 50;
+        emp_wage.rate = 200.0f;
+        emp_wage.calWage();
+        System.out.println("ค่าแรงรวม = " + emp_wage.calWage() + "บาท");
+    }
+}
---
+++ src/ex8/Ex8.java
@@ -0,0 +1,16 @@
+package ex8;
+
+//static method
+class Bonus{
+    public static float calBonus(float s){
+        return 0.05f*s;
+    }
+}
+
+public class Ex8 {
+    public static void main(String[] args) {
+        float bonusAll = Bonus.calBonus(4000);
+        System.out.println("Bonus = " + bonusAll +" บาท");
+    }
+
+}
